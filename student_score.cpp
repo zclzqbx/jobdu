d06c027b49fcf4660565b758202ed14e0f8ed45e
walker <zclzqbx@163.com>
2015-04-10 20:32:20 +0800

    According to the fixed score,find the people

---
+++ student_score.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int N=0;
+	while(cin>>N && N!=0)
+	{
+		vector<int> score(N,0);
+		int i=0,stmp;
+		while(cin>>stmp && i<N)//people can be computed
+		{
+			score[i]=stmp;
+			++i;
+		}
+		int fixed_score;
+		cin>>fixed_score;
+		int num=0;
+		for(int i=0;i<N;++i)
+		{
+			if(score[i]==fixed_score)
+			{
+				num++;
+			}
+		}
+		cout<<num<<endl;
+
+	}
+	return 0;
+}
