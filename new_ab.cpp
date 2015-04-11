2d0e180756632d0bd18c54e37fa05ea14832437b
walker <zclzqbx@163.com>
2015-04-11 19:16:15 +0800

    new a+b

---
+++ new_ab.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+	int a,b,m;
+	while(cin>>m && m!=0) 
+	{
+		cin>>a>>b;
+		vector<int> tmp;
+		int sum=a+b;
+		while(sum!=0)
+		{
+			tmp.push_back(sum%m);
+			sum=sum/m;
+		}
+		for(size_t i=tmp.size()-1;i>=0;--i)
+		{
+			cout<<tmp[i];
+		}
+	}
+	return 0;
+}
